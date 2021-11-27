15fb92f69c6b892de640780df9f853db3bd5a049
hritik1228 <hritikkumar0015@gmail.com>
2021-11-27 18:44:20 +0530

    Added Jio.java

---
+++ spring-demo-ioc/src/com/hr/springIOC/Jio.java
@@ -0,0 +1,17 @@
+package com.hr.springIOC;
+
+public class Jio implements Sim {
+
+	@Override
+	public void calling() {
+		System.out.print("I am calling using my Jio sim");
+
+	}
+
+	@Override
+	public void data() {
+		System.out.print("I am surfing internet using my Jio sim");
+
+	}
+
+}
